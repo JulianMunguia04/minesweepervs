e9109afa2bfc27bc29aea8df4128fc067d7f218c
JulianMunguia04 <julianmunguia04@gmail.com>
2025-08-13 22:46:31 -0600

    PostgreSQL env setup

--- database/postgresdb.js
+++ database/postgresdb.js
@@ -1,15 +1,49 @@
 import { Client } from 'pg';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const client = new Client({
-  host: 'localhost',
-  port: 5432,
-  user: 'postgres',
-  password: 'IL0v3P0stGr3sqlol!%',
-  database: 'minesweepervs',
+  host: process.env.PG_HOST,
+  port: Number(process.env.PG_PORT),
+  user: process.env.PG_USER,
+  password: process.env.PG_PASSWORD,
+  database: process.env.PG_DATABASE,
 });
 
 client.connect()
   .then(() => console.log('✅ Connected to PostgreSQL'))
   .catch(err => console.error('❌ Connection error', err.stack));
+
+export const createTables = async () => {
+  try {
+    const tableQueries = [
+      `CREATE TABLE IF NOT EXISTS test_table (
+        id SERIAL PRIMARY KEY,
+        message TEXT NOT NULL,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+      );`,
+      `CREATE TABLE IF NOT EXISTS users (
+        id SERIAL PRIMARY KEY,
+        google_id VARCHAR(255) NULL,
+        username VARCHAR(50) UNIQUE NOT NULL,
+        password TEXT NOT NULL,
+        first_name VARCHAR(50) NOT NULL,
+        last_name VARCHAR(50) NOT NULL,
+        date_of_birth DATE NOT NULL,
+        profile_picture TEXT NULL,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+      );`
+    ];
+    for (const query of tableQueries) {
+      await client.query(query);
+    }
+    console.log('✅ Tables ensured in DB');
+  } catch (err) {
+    console.error('❌ DB error on table creation:', err.stack);
+  }
+};
+
+//Run to create all tables
+createTables()
 
 export default client;
